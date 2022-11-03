5a37b8b01ac1a85701866392508af15e6ae3864d
mrpor <mrpor@DESKTOP-OMU09PE>
2022-11-03 21:46:03 +0300

    Добавьте файлы проекта.

---
+++ Tortiki/Menu.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tortiki
+{
+    internal class Menu
+    {
+        internal class RazmerTorta
+        {
+            public string bolshoy = " Большой - 2000";
+            public int cena_bolshoy = 2000;
+            public string sredniy = " Средний - 1500";
+            public int cena_sredniy = 1500;
+            public string malenkiy = " Маленький - 1000";
+            public int cena_malenkiy = 1000;
+        }
+        internal class FormaTorta
+        {
+            public string pryamougolnik = " Прямоугольник - 200";
+            public int cena_pryamougalnuk = 200;
+            public string serdce = " Cердечко - 300";
+            public int cena_serdce = 300;
+            public string ponchik = " Пончик - 400";
+            public int cena_ponchik = 400;
+        }
+        internal class KolichestvoKorshey
+        {
+            public string odinkorsh = " 1 корж - 200";
+            public int cena_odinkorsh = 200;
+            public string dvakorsha = " 2 коржа - 400";
+            public int cena_dvakorsha = 400;
+            public string trikorsha = " 3 коржа - 600";
+            public int cena_trikorsha = 600;
+        }
+        internal class VkusTorta
+        {
+            public string klukva = " Клюквенный - 100";
+            public int cena_klukva = 100;
+            public string bananoviy = " Банановый - 175";
+            public int cena_bananoviy = 175;
+            public string chocolate = " Шоколадный - 300";
+            public int cena_chocolate = 300;
+        }
+        internal class DecorTorta
+        {
+            public string mastika = " Мастика - 200";
+            public int cena_mastika = 200;
+            public string svechi = " Свечи - 150";
+            public int cena_svechi = 150;
+            public string photo = " Фото - 500";
+            public int cena_photo = 500;
+        }
+        internal class GlazurTorta
+        {
+            public string vanil = " Ванильная - 100";
+            public int cena_vanil = 100;
+            public string posipka = " Посыпка - 150";
+            public int cena_posipka = 150;
+            public string chokoladnayastruska = " Шоколадная стружка - 250";
+            public int cena_chokoladnayastruska = 250;
+        }
+    }
+}
---
+++ Tortiki/Program.cs
@@ -0,0 +1,272 @@
+﻿using static Tortiki.Menu;
+
+namespace tortiki
+{
+
+    internal class Program
+    {
+        static public ConsoleKeyInfo clavisha;
+        static public string tort;
+        static public int cena;
+        static public int position = 3;
+
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Добро пожаловать в магазин тортов \"Шоколадный Заяц\"!");
+            Console.WriteLine("Соберите свой торт: \n");
+            Console.Write("  1)Форма торта  \n  2)Размер торта  \n  3)Вкус \n  4)Количество коржей  \n  5)Глазурь  \n  6)Декор  \n  7)Конец заказа \n");
+            Console.Write("\n Цена:" + cena + "\n  Ваш торт:" + tort);
+            Strelki();
+            Perecl();
+        }
+        static void Strelki()
+        {
+            position = 3;
+            do
+            {
+                string del = new string(' ', 2);
+                if (clavisha.Key == ConsoleKey.UpArrow)
+                {
+                    Console.SetCursorPosition(0, position--);
+                }
+                else if (clavisha.Key == ConsoleKey.DownArrow)
+                {
+                    Console.SetCursorPosition(0, position++);
+                }
+                Console.Write("\r" + del);
+                Console.SetCursorPosition(0, position);
+                Console.Write("->");
+                clavisha = Console.ReadKey();
+                if (clavisha.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+            } while (clavisha.Key != ConsoleKey.Enter);
+        }
+        static void Perecl()
+        {
+            Console.Clear();
+            Console.WriteLine("Для выхода нажмите Esc\nВыберите пункт из меню\n");
+            if (clavisha.Key == ConsoleKey.Enter)
+            {
+                switch (position)
+                {
+                    case 3:
+                        FormaTorta formatorta = new FormaTorta();
+                        Console.WriteLine("  " + formatorta.pryamougolnik);
+                        Console.WriteLine("  " + formatorta.serdce);
+                        Console.WriteLine("  " + formatorta.ponchik);
+                        position = 3;
+                        Strelki();
+                        if (clavisha.Key == ConsoleKey.Enter)
+                        {
+                            switch (position)
+                            {
+                                case 3:
+                                    cena = cena + formatorta.cena_pryamougalnuk;
+                                    tort = tort + formatorta.pryamougolnik;
+                                    break;
+                                case 4:
+                                    cena = cena + formatorta.cena_serdce;
+                                    tort = tort + formatorta.serdce;
+                                    break;
+                                case 5:
+                                    cena = cena + formatorta.cena_ponchik;
+                                    tort = tort + formatorta.ponchik;
+                                    break;
+                            }
+                        }
+                        if (clavisha.Key == ConsoleKey.Escape)
+                        {
+                            Menu();
+                            return;
+                        }
+                        Menu();
+                        return;
+                    case 4:
+                        RazmerTorta razmertorta = new RazmerTorta();
+                        Console.WriteLine("  " + "Большой - "+ razmertorta.cena_bolshoy);
+                        Console.WriteLine("  " + "Средний - " + razmertorta.cena_sredniy);
+                        Console.WriteLine("  " + "Маленький - " + razmertorta.cena_malenkiy);
+                        position = 3;
+                        Strelki();
+                        if (clavisha.Key == ConsoleKey.Enter)
+                        {
+                            switch (position)
+                            {
+                                case 3:
+                                    cena = cena + razmertorta.cena_bolshoy;
+                                    tort = tort + razmertorta.bolshoy;
+                                    break;
+                                case 4:
+                                    cena = cena + razmertorta.cena_sredniy;
+                                    tort = tort + razmertorta.sredniy;
+                                    break;
+                                case 5:
+                                    cena = cena + razmertorta.cena_malenkiy;
+                                    tort = tort + razmertorta.malenkiy;
+                                    break;
+                            }
+                        }
+                        if (clavisha.Key == ConsoleKey.Escape)
+                        {
+                            Menu();
+                            return;
+                        }
+                        Menu();
+                        return;
+                    case 5:
+                        VkusTorta vkustorta = new VkusTorta();
+                        Console.WriteLine("  " + "Клюквенный - " + vkustorta.cena_klukva);
+                        Console.WriteLine("  " + "Банановый - " + vkustorta.cena_chocolate);
+                        Console.WriteLine("  " + "Шоколадный - " + vkustorta.cena_bananoviy);
+                        position = 3;
+                        Strelki();
+                        if (clavisha.Key == ConsoleKey.Enter)
+                        {
+                            switch (position)
+                            {
+                                case 3:
+                                    cena = cena + vkustorta.cena_klukva;
+                                    tort = tort + vkustorta.klukva;
+                                    break;
+                                case 4:
+                                    cena = cena + vkustorta.cena_chocolate;
+                                    tort = tort + vkustorta.chocolate;
+                                    break;
+                                case 5:
+                                    cena = cena + vkustorta.cena_bananoviy;
+                                    tort = tort + vkustorta.bananoviy;
+                                    break;
+                            }
+                        }
+                        if (clavisha.Key == ConsoleKey.Escape)
+                        {
+                            Menu();
+                            return;
+                        }
+                        Menu();
+                        return;
+                    case 6:
+                        position = 3;
+                        KolichestvoKorshey kolichestvokorshey = new KolichestvoKorshey();
+                        Console.WriteLine("  " + "1 корж - " + kolichestvokorshey.cena_odinkorsh);
+                        Console.WriteLine("  " + "2 коржа - " + kolichestvokorshey.cena_dvakorsha);
+                        Console.WriteLine("  " + "3 коржа - " + kolichestvokorshey.cena_trikorsha);
+                        position = 3;
+                        Strelki();
+                        if (clavisha.Key == ConsoleKey.Enter)
+                        {
+                            switch (position)
+                            {
+                                case 3:
+                                    cena = cena + kolichestvokorshey.cena_odinkorsh;
+                                    tort = tort + kolichestvokorshey.odinkorsh;
+                                    break;
+                                case 4:
+                                    cena = cena + kolichestvokorshey.cena_dvakorsha;
+                                    tort = tort + kolichestvokorshey.dvakorsha;
+                                    break;
+                                case 5:
+                                    cena = cena + kolichestvokorshey.cena_trikorsha;
+                                    tort = tort + kolichestvokorshey.trikorsha;
+                                    break;
+                            }
+                        }
+                        if (clavisha.Key == ConsoleKey.Escape)
+                        {
+                            Menu();
+                            return;
+                        }
+                        Menu();
+                        return;
+                    case 7:
+                        position = 3;
+                        GlazurTorta glazurtorta = new GlazurTorta();
+                        Console.WriteLine("  " + "Ванильная - " + glazurtorta.cena_vanil);
+                        Console.WriteLine("  " + "Посыпка - " + glazurtorta.cena_posipka);
+                        Console.WriteLine("  " + "Шоколадная стружка - " + glazurtorta.cena_chokoladnayastruska);
+                        position = 3;
+                        Strelki();
+                        if (clavisha.Key == ConsoleKey.Enter)
+                        {
+                            switch (position)
+                            {
+                                case 3:
+                                    cena = cena + glazurtorta.cena_vanil;
+                                    tort = tort + glazurtorta.vanil;
+                                    break;
+                                case 4:
+                                    cena = cena + glazurtorta.cena_posipka;
+                                    tort = tort + glazurtorta.posipka;
+                                    break;
+                                case 5:
+                                    cena = cena + glazurtorta.cena_chokoladnayastruska;
+                                    tort = tort + glazurtorta.chokoladnayastruska;
+                                    break;
+                            }
+                        }
+                        if (clavisha.Key == ConsoleKey.Escape)
+                        {
+                            Menu();
+                            return;
+                        }
+                        Menu();
+                        return;
+                    case 8:
+                        position = 3;
+                        DecorTorta decortorta = new DecorTorta();
+                        Console.WriteLine("  " + "Мастика - " + decortorta.cena_mastika);
+                        Console.WriteLine("  " + "Свечи - " + decortorta.cena_svechi);
+                        Console.WriteLine("  " + "Фото - " + decortorta.cena_photo);
+                        position = 3;
+                        Strelki();
+                        if (clavisha.Key == ConsoleKey.Enter)
+                        {
+                            switch (position)
+                            {
+                                case 3:
+                                    cena = cena + decortorta.cena_mastika;
+                                    tort = tort + decortorta.mastika;
+                                    break;
+                                case 4:
+                                    cena = cena + decortorta.cena_svechi;
+                                    tort = tort + decortorta.svechi;
+                                    break;
+                                case 5:
+                                    cena = cena + decortorta.cena_photo;
+                                    tort = tort + decortorta.photo;
+                                    break;
+                            }
+                        }
+                        if (clavisha.Key == ConsoleKey.Escape)
+                        {
+                            Menu();
+                            return;
+                        }
+                        Menu();
+                        return;
+                    case 9:
+                        Console.Clear();
+                        Console.WriteLine("Благодарим за заказ! Чтобы сделать еще один нажмите Esc");
+                        string data = DateTime.Now.ToString("dddd, dd MMMM yyyy");
+                        File.AppendAllText("C:\\Users\\mrpor\\Desktop\\Заказ.txt", "Текущая дата: " + data + "\n" + "Ваш заказ: " + tort + "\n" + "Цена: " + cena + "\n");
+                        clavisha = Console.ReadKey();
+                        if (clavisha.Key == ConsoleKey.Escape)
+                        {
+                            cena = 0;
+                            tort = " ";
+                            Menu();
+                            return;
+                        }
+                        return;
+                }
+            }
+        }
+    }
+}
